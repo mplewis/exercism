7fb9240307a5561ea8e81e680928b89eb137d4ac
Matt Lewis <matt@mplewis.com>
2018-01-10 20:23:01 -0700

    cleanup: inline fns

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -2,13 +2,11 @@ open Core
 
 type nucleotide = A | C | G | T
 
-let tuple_eq (a, b) = a = b
-let is_false a = not a
-
 let hamming_distance (left:nucleotide list) (right:nucleotide list): int option =
   match (List.zip left right) with
   | None -> None
   | Some zipped -> Some (
-      List.map zipped tuple_eq |>
-      List.count ~f:is_false
+      zipped |>
+      List.map ~f:(fun (a, b) -> a = b) |>
+      List.count ~f:(fun x -> not x)
     )
